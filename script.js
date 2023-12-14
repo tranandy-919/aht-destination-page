4a65ec74e85314d94e378842abfbe6aa11812c1f
Andy Tran <andy.tran@tlgcohort.com>
2023-12-14 14:21:36 -0800

    submissions now create cards

--- script.js
+++ script.js
@@ -13,5 +13,37 @@ user_input_form.addEventListener("submit", (e) => {
     // clear the form
     user_input_form.reset();
 
-    console.log(destinationName, locationName, photoUrl, descript)
+    const card = createCard({destinationName, locationName, photoUrl, descript});
+    cards_container.appendChild(card);
 });
+
+function createCard({destinationName, locationName, photoUrl, descript}) {
+    /*
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    */
+    const card =  document.createElement("div");
+    card.classList.add("card")
+    card.setAttribute("style", "width: 18rem;")
+    card.innerHTML =  `
+        <img src=${photoUrl} class="card-img-top" alt=${destinationName} at ${locationName}>
+        <div class="card-body">
+            <h5 class="card-title">${destinationName}</h5>
+            <p class="card-text">${locationName}</p>
+            ${descript && '<p class="card-text">${descript}</p>'}
+            <button type="button" class="btn btn-info">Edit</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+        `
+    
+    
+
+
+    return card;
+}
